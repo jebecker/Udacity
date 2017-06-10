09eafbdb59ca953a482daa8e4e8ed97083a99c9f
Jayme Becker <beckerjayme@gmail.com>
2017-06-10 10:23:01 -0500

    Finished creating the Main Meme View Controller. That includes adding the functionality to the press of the camera button, album button, and share button. Added the correct notifications needed to show/hide the keyboard properly.

--- MemeMe/MemeMe/MainMemeMeViewController.swift
+++ MemeMe/MemeMe/MainMemeMeViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MainMemeMeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MainMemeMeViewController: UIViewController, UINavigationControllerDelegate, UITextFieldDelegate {
 
     // Declare outlets for all of my UI Elements I will need to be able to access 
     @IBOutlet weak var cancelButton: UIBarButtonItem!
@@ -18,26 +18,63 @@ class MainMemeMeViewController: UIViewController, UIImagePickerControllerDelegat
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var toolbar: UIToolbar!
+    @IBOutlet weak var navBarItems: UINavigationItem!
+    
+    // Declare custom variable for the image picker delegate
+    var imagePickerDelegate: MemeMeImagePickerDelegate?
   
     // Declare IBActions for the buttons on the screen
     @IBAction func shareButtonWasPressed(_ sender: Any) {
+        
+        // generate the memed image
+        let memedImage = generateMemedImage()
+        let activityViewController = UIActivityViewController(activityItems: [memedImage], applicationActivities:nil)
+        present(activityViewController, animated: true, completion: nil)
+        
+        activityViewController.completionWithItemsHandler = {(type,completed,item,error) in
+            
+            if completed {
+                self.save(memedImage: memedImage)
+            }
+        
+        }
     }
     
-    
+    // Return the view to its original state if a user taps the cancel button
     @IBAction func cancelButtonPressed(_ sender: Any) {
+        clearView()
     }
     
+    // IBAction that will present the camera to the user to allow them to take a picture they want to meme
     @IBAction func cameraButtonPressed(_ sender: Any) {
+    
+        // bring up the phones camera so the user can take a picture
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = imagePickerDelegate
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+        
     }
     
     // IBAction that will present the camera album to the user to allow them to pick the picture they want to meme
     @IBAction func albumButtonPressed(_ sender: Any) {
-        
+       
+        // bring up the camera roll so the user can pick a picture
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = imagePickerDelegate
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // assign the textfields delegate to UITextFieldDelegate
+        topTextField.delegate = self
+        bottomTextField.delegate = self
+        imagePickerDelegate = MemeMeImagePickerDelegate(imageView: imageView)
         
         // call the configureUI method
         configureUI()
@@ -47,10 +84,17 @@ class MainMemeMeViewController: UIViewController, UIImagePickerControllerDelegat
     override func viewWillAppear(_ animated: Bool) {
         
         // check whether the device has a camera, if not, disable the camera button
-        
         if !UIImagePickerController.isSourceTypeAvailable(.camera) {
             cameraButton.isEnabled = false
         }
+        
+        // subscribe to the keyboard notifications
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        // unsubscribe to the keyboard notifications
+        unsubscribeToKeyboardNotifications()
     }
     
     
@@ -66,6 +110,121 @@ class MainMemeMeViewController: UIViewController, UIImagePickerControllerDelegat
         bottomTextField.defaultTextAttributes = memeTextAtrributes
         topTextField.textAlignment = .center
         bottomTextField.textAlignment = .center
+        imageView.contentMode = .scaleAspectFit
+    }
+    
+    // custom method to clear the view if the cancel button is pressed
+    func clearView() {
+        imageView.image = nil
+        topTextField.text = "TOP"
+        bottomTextField.text = "BOTTOM"
+        topTextField.resignFirstResponder()
+        bottomTextField.resignFirstResponder()
+    }
+    
+   // MARK: - Set up the notification center to start observiing for the keyboard to either show/hide it
+    
+    func subscribeToKeyboardNotifications() {
+       
+        // Add an observer to the notification center to fire the 'keyboardWillShow' method
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: .UIKeyboardWillShow, object: nil)
+        
+        // Add an observer for the keyboardWillHide method in order to bring the view back to its original position
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeToKeyboardNotifications() {
+       
+        // Remove the observer in the Notification center that controls the .UIKeyboardWillShow/hide notification
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
+    
+     // MARK: - Custom Methods to show and hide the keyboard corerctly
+    
+    func keyboardWillShow(notification: Notification) {
+        
+        // adjust the view for the keyboard so nothing gets cut off for the bottom text field only
+        
+        if bottomTextField.isEditing {
+            view.frame.origin.y -= getKeyboardHeight(notification: notification)
+        }
         
     }
+    
+    func keyboardWillHide(notification: Notification) {
+        
+        // adjust the view back to its original position
+        view.frame.origin.y = 0
+    }
+    
+    func getKeyboardHeight(notification: Notification) -> CGFloat {
+        
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        
+        return (keyboardSize.cgRectValue.height)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    // MARK: - Method to generate/save the memed image
+    
+    // Method to generate the memed image
+    func generateMemedImage() -> UIImage {
+        
+        // hide the toolbar and nav bar first so that way we can capture the screen with the image and text without having the toolbar and nav bar in the memed image
+        toolbar.isHidden = true
+        navigationController?.navigationBar.isHidden = true
+        
+        // Render the view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        // no re-show the toolbar and nav bar
+        toolbar.isHidden = false
+        navigationController?.navigationBar.isHidden = false
+        
+        return memedImage
+    }
+    
+    func save(memedImage: UIImage) {
+        // create the meme 
+        let meme = Meme(topText: topTextField.text!, bottomText: bottomTextField.text!, originalImage: imageView.image!, memedImage: memedImage)
+    }
+    
+}
+
+
+
+// MARK: - Custom Delegate class for UIImagePickerDelegate that way we keep the VC doing as little as possible (Do one thing, do it well. (https://vimeo.com/204897590)
+
+class MemeMeImagePickerDelegate: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // Delcare properties needed to save/create a Memed Image
+    var imageView: UIImageView
+
+    // Custom Initializer 
+    init(imageView: UIImageView) {
+        self.imageView = imageView
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // grab the image that was selected
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
 }
